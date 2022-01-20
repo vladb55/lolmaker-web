bb0daadcc7804ca5e9fa279290bc4c54e2a6b8b5
vladb55 <stephosxox@gmail.com>
2022-01-20 17:33:18 +0000

    Deploying to gh-pages from @ vladb55/lolmaker-web@9034fc4f45092a87930f7e0821837b48ce923857 🚀

---
+++ _next/static/mSmd60N52-6pmQ2bIjwR_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/716-b572e64561c653ec.js","static/chunks/pages/index-2441b3f486ab4d7a.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
