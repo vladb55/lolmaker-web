7e3f01c7aa69cfbcc45045a297d61e35bde8384f
Vlad <stephosxox@gmail.com>
2022-01-20 23:31:50 +0400

    change next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/lolmaker-web' : undefined;
+const assetPrefix = isProduction ? '/lolmaker-web' : undefined;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath,
+  assetPrefix,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
